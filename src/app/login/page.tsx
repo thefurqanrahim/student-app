0f38b3043b4c6e23f4ad90b8ef8798dbed5d41dd
thefurqanrahim <furquan,rahim124@gmail.com>
2024-09-27 11:48:32 +0500

    type bug fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
     e.preventDefault();
     setError('');
 
-    const storedUsers = JSON.parse(localStorage.getItem('users') || '[]') as { email: string; password: string; }[];
+    const storedUsers: { email: string; password: string }[] = JSON.parse(localStorage.getItem('users') || '[]');
     const user = storedUsers.find((user: any) => user.email === data.email && user.password === data.password);
 
     if (user) {
